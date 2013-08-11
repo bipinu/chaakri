37e68cdced629b52e84b14204969ae985975c9dd
Bipin Upadhyay <om@projectbee.org>
2013-08-11 17:32:01 +0530

    added logo upload folder to gitignore

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -1,64 +1,70 @@
 <?php /* @var $this Controller */ ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="language" content="en" />
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="en" />
 
-	<!-- blueprint CSS framework -->
-	<!--<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />-->
-	<!--<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />-->
-	<!--[if lt IE 8]>
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
-	<![endif]-->
-
-	<!--<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />-->
-	<!--<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />-->
         <?php Yii::app()->bootstrap->register(); ?>
+        <!-- blueprint CSS framework -->
+        <!--<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />-->
+        <!--<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />-->
+        <!--[if lt IE 8]>
+        <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
+        <![endif]-->
 
-	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
-</head>
+        <!--<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />-->
+        <!--<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />-->
 
-<body>
+        <title><?php echo CHtml::encode($this->pageTitle); ?></title>
+    </head>
 
-<div class="container" id="page">
+    <body>
 
-	<div id="header">
-		<div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
-	</div><!-- header -->
+        <div class="container" id="page">
 
-	<div id="mainmenu">
-		<?php $this->widget('zii.widgets.CMenu',array(
-			'items'=>array(
-				array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-				array('label'=>'Contact', 'url'=>array('/site/contact')),
-				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
-				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
-			),
-		)); ?>
-	</div><!-- mainmenu -->
-	<?php if(isset($this->breadcrumbs)):?>
-		<?php $this->widget('zii.widgets.CBreadcrumbs', array(
-			'links'=>$this->breadcrumbs,
-		)); ?><!-- breadcrumbs -->
-	<?php endif?>
+            <div id="header">
+                <div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
+            </div><!-- header -->
 
-	<?php echo $content; ?>
-<?php $this->widget('bootstrap.widgets.TbButton', array(
-    'label'=>'Primary',
-    'type'=>'primary', // null, 'primary', 'info', 'success', 'warning', 'danger' or 'inverse'
-    'size'=>'large', // null, 'large', 'small' or 'mini'
-)); ?>
-	<div class="clear"></div>
+            <div id="mainmenu">
+                <?php
+                $this->widget('zii.widgets.CMenu', array(
+                    'items' => array(
+                        array('label' => 'Home', 'url' => array('/site/index')),
+                        array('label' => 'About', 'url' => array('/site/page', 'view' => 'about')),
+                        array('label' => 'Contact', 'url' => array('/site/contact')),
+                        array('label' => 'Login', 'url' => array('/site/login'), 'visible' => Yii::app()->user->isGuest),
+                        array('label' => 'Logout (' . Yii::app()->user->name . ')', 'url' => array('/site/logout'), 'visible' => !Yii::app()->user->isGuest)
+                    ),
+                ));
+                ?>
+            </div><!-- mainmenu -->
+            <?php if (isset($this->breadcrumbs)): ?>
+                <?php
+                $this->widget('zii.widgets.CBreadcrumbs', array(
+                    'links' => $this->breadcrumbs,
+                ));
+                ?><!-- breadcrumbs -->
+            <?php endif ?>
 
-	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by Chaakriii.<br/>
-		All Rights Reserved.<br/>
-		<?php echo Yii::powered(); ?> & <a href="https://github.com/bipinu/chaakri/">Chaakri</a>
-	</div><!-- footer -->
+            <?php echo $content; ?>
+            <?php
+            $this->widget('bootstrap.widgets.TbButton', array(
+                'label' => 'Primary',
+                'type' => 'primary', // null, 'primary', 'info', 'success', 'warning', 'danger' or 'inverse'
+                'size' => 'large', // null, 'large', 'small' or 'mini'
+            ));
+            ?>
+            <div class="clear"></div>
 
-</div><!-- page -->
+            <div id="footer">
+                Copyright &copy; <?php echo date('Y'); ?> by Chaakriii.<br/>
+                All Rights Reserved.<br/>
+<?php echo Yii::powered(); ?> & <a href="https://github.com/bipinu/chaakri/">Chaakri</a>
+            </div><!-- footer -->
 
-</body>
+        </div><!-- page -->
+
+    </body>
 </html>
